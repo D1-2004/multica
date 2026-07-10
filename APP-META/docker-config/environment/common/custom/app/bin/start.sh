b1ea101e1f16e7ba8a0b97384398f0e1b8c6e9c5
xumo <weinan.hwn@alibaba-inc.com>
2026-07-10 13:02:57 +0800

    fix: restore aone nginx startup flow

--- APP-META/docker-config/environment/common/custom/app/bin/start.sh
+++ APP-META/docker-config/environment/common/custom/app/bin/start.sh
@@ -3,5 +3,5 @@
 start_app() {
   local app_root="/home/admin/${APP_NAME}"
   echo "start ${APP_NAME} from ${app_root}"
-  exec bash "${app_root}/target/${APP_NAME}/src/main.sh"
+  bash "${app_root}/target/${APP_NAME}/src/main.sh"
 }
